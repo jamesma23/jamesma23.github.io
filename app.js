798381b8b4d6de74f21acb2634f932ae27557b68
James Ma <jamesma@berkeley.edu>
2020-12-25 04:35:38 -0800

    hihi

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-var x = 0
-while (x < 100) {
-  document.getElementById('head').style.background = ''
-  document.getElementById('head').style.background = 'blue'
-  x = x + 1
+function increment (i) {
+  return (i + 1)
 }
+var languages = ['python', 'java', 'c', 'r', 'html', 'css', 'javascript', 'scheme', 'matlab', 'sql', 'premiere pro']
+var i = 0
+function disappear () {
+  setTimeout(increment, 10000)
+  i = increment(i)
+  document.getElementById(languages[i]).style.display = 'none'
+  if (i === 11) {
+    i = 0
+  }
+}
+disappear()
