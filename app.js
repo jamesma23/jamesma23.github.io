f5572ad9e4db8fc239757e6d3b7603a883437884
james <jamesma@berkeley.edu>
2020-12-24 19:50:04 -0800

    my first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var x = 0
+while (x < 100) {
+  document.getElementById('head').style.background = ''
+  document.getElementById('head').style.background = 'blue'
+  x = x + 1
+}
---
+++ name.py
@@ -0,0 +1,7 @@
+def function (name):
+  answer = ''
+  for i in name:
+    answer = i+answer
+  return answer
+
+print(function("james"))
